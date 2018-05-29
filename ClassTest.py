60a52afd72cea22e9d346784ae33c267a54bf069
dhr1676 <ding.h@northeastern.edu>
2018-05-29 16:09:27 +0800

    Initial Commit

--- ClassTest.py
+++ ClassTest.py
@@ -1,34 +1,51 @@
-# 类的定义与使用
+# 类的定义
 class Human(object):
-    name = "no name"
-
-    def __init__(self, name="no one"):
-        self.name = name
+    # 实例的变量名以"__"开头，则变为私有变量
+    def __init__(self, name="no one", gender="Unknown", age=0):
+        self.__name = name
+        self.__gender = gender
+        self.age = age
 
     def __del__(self):
-        print("I, %s, am destroyed" % self.name)
+        print("I, %s, am destroyed\n" % self.__name)
 
-    def SayName(self):
-        print("My name is %s" % self.name)
+    def SayHi(self):
+        print("My name is %s, my gender is %s \n" % (self.__name, self.__gender))
 
-# 类的继承
-class Student(Human):
 
-    def Read(self):
-        print("%s is reading" % self.name)
+# 类的定义与使用
+class Student(object):
+
+    def __init__(self, name="no one", score=0):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print("%s : %s" % (self.__name, self.__score))
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, new_score):
+        if 0 <= new_score <= 100:
+            self.__score = new_score
+        else:
+            raise ValueError("Wrong score!")
 
 
 if __name__ == '__main__':
-    tom = Human("Tom")
-    tom.SayName()
-    # tom.__del__()
+    tom = Human("Tom", "boy", 10)
+    tom.SayHi()
     del tom
 
-    jerry = Human()
-    jerry.SayName()
-    # jerry.__del__()
+    jerry = Student("Jerry", 90)
+    jerry.print_score()
+    # jerry.set_score(999)
     del jerry
 
-    student = Student("Ding")
-    student.Read()
-    del student
+    # student = Student("Ding")
+    # student.Read()
+    # del student
