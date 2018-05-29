20fda93d4111e1b110732ba76d38710e7e883a00
dhr1676 <ding.h@northeastern.edu>
2018-05-29 23:54:47 +0800

    Initial Commit

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,11 +1,11 @@
 class Node(object):
 
-    def __init__(self, left=None, right=None, parent=None, key=None, data=None):
-        self._left = left
-        self._right = right
-        self._parent = parent
-        self._key = key
-        self._data = data
+    def __init__(self, key=None, data=None):
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.key = key
+        self.data = data
 
 
 class Tree(object):
@@ -19,24 +19,45 @@ class Tree(object):
     def SearchAux(self, key):
         return None
 
-    def PrintAux(self, node):
+    def __PrintAux(self, node):
         if node is None:
             return
 
-        self.PrintAux(node.left)
-        print("\n")
-        print("Key: ", end="")
-
-        return None
+        self.__PrintAux(node.left)
+        print("Key is: %d, value is %d\n" % (node.key, node.data))
+        self.__PrintAux(node.right)
 
     def Transplant(self, u, v):
         return None
 
     def PrintTree(self):
+        self.__PrintAux(self._root)
         return
 
     def Insert(self, key, data):
-        return
+        node = Node()
+        node.key = key
+        node.data = data
+
+        x = self._root
+        y = None
+        while x.key is not None:
+            y = x
+            if x.data > data:
+                x = x.left
+                print(type(x))
+            else:
+                x = x.right
+                print(type(x))
+
+
+        node.parent = y
+        if y is None:
+            self._root = node
+        elif y.data > data:
+            y.left = node
+        else:
+            y.right = node
 
     def MinNode(self):
         return
@@ -46,3 +67,14 @@ class Tree(object):
 
     def Delete(self, key):
         return
+
+
+if __name__ == '__main__':
+    tree = Tree()
+    tree.Insert(1, 1)
+    tree.Insert(2, 2)
+    # tree.Insert(3, 3)
+    # tree.Insert(4, 4)
+    # tree.Insert(5, 5)
+    # tree.Insert(6, 6)
+    tree.PrintTree()
