4ce094f99075daf9ff39585275277cdf9fc84370
dhr1676 <ding.h@northeastern.edu>
2018-05-29 17:41:27 +0800

    Initial Commit

--- ClassTest.py
+++ ClassTest.py
@@ -19,9 +19,21 @@ class Student(object):
     def __init__(self, name="no one", score=0):
         self.__name = name
         self.__score = score
+        # 需要注意的是，在Python中，变量名类似__xxx__的，也就是以双下划线开头，
+        # 并且以双下划线结尾的，是特殊变量，特殊变量是可以直接访问的，
+        # 不是private变量，所以，不能用__name__、__score__这样的变量名。
+        #
+        # 有些时候，你会看到以一个下划线开头的实例变量名，比如_name，
+        # 这样的实例变量外部是可以访问的，但是，按照约定俗成的规定，
+        # 当你看到这样的变量时，意思就是，“虽然我可以被访问，
+        # 但是，请把我视为私有变量，不要随意访问”。
+        #
+        # 双下划线开头的实例变量是不是一定不能从外部访问呢？
+        # 其实也不是。不能直接访问__name是因为Python解释器对外把__name变量改成了_Student__name，
+        # 所以，仍然可以通过_Student__name来访问__name变量
 
     def print_score(self):
-        print("%s : %s" % (self.__name, self.__score))
+        print("%s : %s\n" % (self.__name, self.__score))
 
     def get_name(self):
         return self.__name
@@ -30,10 +42,29 @@ class Student(object):
         return self.__score
 
     def set_score(self, new_score):
+        # 参数检查
         if 0 <= new_score <= 100:
             self.__score = new_score
         else:
             raise ValueError("Wrong score!")
+
+
+# 继承和多态
+class Animal(object):
+
+    def __init__(self, name="Animal"):
+        self.__name = name
+
+    def run(self):
+        print("%s is running" % self.__name)
+
+    def getName(self):
+        return self.__name
+
+
+class Dog(Animal):
+    def run(self):
+        print("Dog %s is running" % self.getName())
 
 
 if __name__ == '__main__':
@@ -45,6 +76,9 @@ if __name__ == '__main__':
     jerry.print_score()
     # jerry.set_score(999)
     del jerry
+
+    snoopy = Dog("Snoopy")
+    snoopy.run()
 
     # student = Student("Ding")
     # student.Read()
--- LinkedList.py
+++ LinkedList.py
@@ -1,24 +1,107 @@
 class Node(object):
 
-    def __init__(self, value=0, next = None):
+    def __init__(self, value=0, p=None):
         self.data = value
-        self.__next = next
+        self.next = p
 
 
 class LinkedList(object):
 
     def __init__(self):
-        self.head = 0
+        self.head = Node()
 
-    def __del__(self):
-        print("Linked List has been destroyed!")
+    def buildList(self, data):
+
+        self.head = Node(data[0])
+        p = self.head
+        for i in data[1:]:
+            node = Node(i)
+            p.next = node
+            p = p.next
+
+    def getSize(self):
+        now = self.head
+        size = 0
+        while now:
+            size += 1
+            now = now.next
+
+        return size
+
+    def is_empty(self):
+        if self.getSize() == 0:
+            return True
+        else:
+            return False
+
+    def clearList(self):
+        self.head = 0
+        print("List is empty now!")
+
+    def insert(self, index, value):
+        if index == 0:
+            q = Node(value, self.head)
+            self.head = q
+
+        p = self.head
+        return None
+
+    def remove(self, key):
+        if self.is_empty():
+            raise ValueError("Empty list!")
+
+        dummy = Node(-1)
+        dummy.next = self.head
+        temp = dummy
+
+        while temp and temp.next:
+            if temp.next.data == key:
+                temp.next = temp.next.next
+                print("Remove node %d\n" % key)
+                return
+            else:
+                temp = temp.next
+
+        print("Node not found")
+        return None
 
     def getNode(self, key):
 
-        if self.head is None:
-            print("Linked list is empty")
+        if self.is_empty():
+            print("List is empty")
             return None
-        else:
-            return self.getNode(key)
 
-    @property
+        temp = self.head
+        index = 1
+        while temp:
+            if key == temp.data:
+                return index
+            temp = temp.next
+            index += 1
+
+        print("Node not found")
+        return None
+
+    # Traverse the list
+    def printList(self):
+        temp = self.head
+        print("The list is: ", end="")
+        while temp:
+            print("%d" % temp.data, end="")
+            if temp.next:
+                print(" --> ", end="")
+            temp = temp.next
+        print("\n")
+
+
+if __name__ == '__main__':
+    input_data = [1, 3, 5, 7, 9]
+    print(input_data)
+    link_list = LinkedList()
+    link_list.buildList(input_data)
+    link_list.printList()
+
+    print("3 is the %d element\n" % link_list.getNode(3))
+
+    link_list.remove(3)
+    link_list.printList()
